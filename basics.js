7d631d8434e032e98469f708abdb91b78ee8ae0f
michael Sias <michaelsias@michaels-mbp.attlocal.net>
2021-03-11 17:16:20 -0500

    self Destruct message

---
+++ basics.js
@@ -0,0 +1,13 @@
+// 1. use a alert promt to send the user the self-destruct message
+alert("Warning!! This message will self-destruct in");
+
+// 2. use alert messages for the count down
+alert("....3");
+alert("....2");
+alert("....1");
+
+// 3. This statment selects the <h1> element and replaces its text with "BOOM!"
+document.querySelector("h1").textContent = "🔥BOOM!🔥";
+
+// 4. Log "Message destroyed!" to the console
+console.log("Messaged destroyed!")
